fd8df41a69dcfcaca975e2c8c1b99bb39f87b24d
Zarzuket <davide.bajo@gmail.com>
2021-07-28 15:06:40 +0200

    Exercise JS

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var name = prompt('Insert your nome');
+var surname = prompt ('Insert your surname');
+var color = prompt('Insert your favourite color')
+var result = name + surname + color + "40";
+document.getElementById('psw').innerHTML = result;
+
+
