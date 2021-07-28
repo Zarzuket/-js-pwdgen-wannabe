e1bb3c8fdbed94662249b17cad5c9794bd073fd3
Zarzuket <davide.bajo@gmail.com>
2021-07-28 15:08:35 +0200

    add title

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@ var name = prompt('Insert your nome');
 var surname = prompt ('Insert your surname');
 var color = prompt('Insert your favourite color')
 var result = name + surname + color + "40";
-document.getElementById('psw').innerHTML = result;
+document.getElementById('psw').innerHTML += result;
 
 
